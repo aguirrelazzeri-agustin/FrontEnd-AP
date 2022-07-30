22b5662322f4c2529d84178284a1bef8937aa6bb
Agustin Aguirre Lazzeri <95191761+Lazzeriagustin@users.noreply.github.com>
2022-07-30 20:47:06 -0300

    viendo si anda app

--- Frontend/src/app/service/auth.service.ts
+++ Frontend/src/app/service/auth.service.ts
@@ -9,7 +9,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL = 'https://miportafolio-aal.herokuapp.com/auth/';
+  authURL = 'https://miportafolio-aal.herokuapp.com/auth/login';
 
   constructor(private httpClient: HttpClient) { }
 
