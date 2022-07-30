cd3c78029c9bb833b74e94a538b0d3b5fd99867d
Agustin Aguirre Lazzeri <95191761+Lazzeriagustin@users.noreply.github.com>
2022-07-30 19:04:21 -0300

    viendo error de app

--- Frontend/src/app/service/s-experiencia.service.ts
+++ Frontend/src/app/service/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-    expURL = 'https://miportafolio-aal.herokuapp.com/';
+    expURL = 'https://miportafolio-aal.herokuapp.com';
   
     constructor(private httpClient: HttpClient) { }
   
--- Frontend/src/app/service/skills.service.ts
+++ Frontend/src/app/service/skills.service.ts
@@ -7,7 +7,7 @@ import { Skills } from '../model/skills';
   providedIn: 'root'
 })
 export class SkillsService {
-  URLski = 'https://miportafolio-aal.herokuapp.com/';
+  URLski = 'https://miportafolio-aal.herokuapp.com';
 
   constructor(private httpClient: HttpClient) { }
 
