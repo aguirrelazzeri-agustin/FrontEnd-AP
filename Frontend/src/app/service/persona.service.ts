96f75f0502a73251c6480f7c222b9aaef3729f1f
Agustin Aguirre Lazzeri <95191761+Lazzeriagustin@users.noreply.github.com>
2022-07-31 15:05:41 -0300

    probando deploy

--- Frontend/src/app/service/persona.service.ts
+++ Frontend/src/app/service/persona.service.ts
@@ -10,25 +10,25 @@ import { persona } from '../model/persona.model';
 export class PersonaService {
   URL = 'https://miportafolio-aal.herokuapp.com/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpCliente: HttpClient) { }
   public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL+'traer/perfil')
+    return this.httpCliente.get<persona>(this.URL+'traer/perfil')
 
   }
 
   public detail(id: number): Observable<persona> {
-    return this.http.get<persona>(this.URL + `detail/${id}`);
+    return this.httpCliente.get<persona>(this.URL + `detail/${id}`);
   }
 
   public save(persona: persona): Observable<any> {
-    return this.http.post<any>(this.URL + 'create', persona);
+    return this.httpCliente.post<any>(this.URL + 'create', persona);
   }
 
   public update(id: number, persona: persona): Observable<any> {
-    return this.http.put<any>(this.URL + `update/${id}`, persona);
+    return this.httpCliente.put<any>(this.URL + `update/${id}`, persona);
   }
 
   public delete(id: number): Observable<any> {
-    return this.http.delete<any>(this.URL + `delete/${id}`);
+    return this.httpCliente.delete<any>(this.URL + `delete/${id}`);
   }
 }
